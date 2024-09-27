997a009cf0c392449af9d11cacbdb872e353fa87
Pratik Kumar <70286186+Pratik-Kumar-621@users.noreply.github.com>
2024-09-28 00:18:13 +0530

    Authentication Complete

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="min-h-screen h-full flex items-center justify-center px-10 py-8">
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 export const montserrat = Montserrat({
@@ -20,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${montserrat.variable} antialiased`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={montserrat.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
